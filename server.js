ffa4816375d346b412219e2a520c5d77f4d62723
José Ricardo Ventrilho <Jos Ricardo Ventrilho>
2016-11-09 12:54:09 -0200

    Atalho para os links e carregamento da index.html

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	var dir = '/views';
+	var file = '/index.html';
+	res.sendFile( __dirname + dir + file );
 });
 
 app.listen(8081, function() {
