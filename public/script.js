1adc45415bdc08e08e74ebb607d698edd1f57c1d
José Ricardo Ventrilho <Jos Ricardo Ventrilho>
2016-11-17 09:30:24 -0200

    Adicionando funções de login

--- public/script.js
+++ public/script.js
@@ -1,30 +1,43 @@
-var name;
+/*
+ * Javascript File
+ */
 
 function getName() {
   var name = list_names[Math.floor(Math.random() * list_names.length)];
   return name;
 }
 
-name = getName();
+var disable_btn_login = function() {
+  name = $('#usr').val();
+  if(name == '') {
+    $('#btn_login').addClass('disabled');
+  } else {
+    $('#btn_login').removeClass('disabled');
+  }
+}
 
+var login = function() {
+  name = $('#usr').val();
+  if(name != '') {
+    $('#myModal').modal('hide');
+    socket.emit('adduser', name);
+  }
+}
+
+var name = getName();
 var socket = io.connect('/');
 
-//arquivo scrips que estava na index.html
 $(function () {
-    $(document).ready(function(){
-	  $('#usr').val(name);
+  $(document).ready(function(){
+
+    $('#usr').val(name);
+    $('#myModal').modal('show');
+
+    $('a').click(function(){
       $('#myModal').modal('show');
-      $('a').click(function(){
-        $('#myModal').modal('show');
-      });
-	  $('#btn_login').click(function(){
-		name = $('#usr').val();
-		if(name != '') {
-			//$('#myModal').modal('hide');
-			socket.emit('adduser', name);
-		}
-	  });
     });
+
+  });
 });
 
 socket.on('user exist', function (n) {
@@ -34,5 +47,3 @@ socket.on('user exist', function (n) {
 socket.on('user add', function (n) {
   $('#myModal').modal('hide');
 });
-
-
