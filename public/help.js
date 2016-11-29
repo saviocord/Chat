53a7d4bd8755aa3c139b785ca991000984b6a426
saviocord <saviocord@gmail.com>
2016-11-29 14:07:18 -0200

    add informaçoes no help

--- public/help.js
+++ public/help.js
@@ -4,15 +4,24 @@ help =
         '<div class="modal-content">' +
           '<div class="modal-header">' +
 						'<button type="button" class="close" data-dismiss="modal">&times;</button>' +
-            '<h4 class="modal-title">Diálogos possíveis</h4>' +
+            '<h4 class="modal-title">Topicos de Conversas com o Chat</h4>' +
           '</div>' +
           '<div class="modal-body">' +
-						'<p>' +
-							'<strong>' + 'Olá' + ' </strong>' +
-						'</p>' +
-						'<p>' +
-							'<strong>' + 'O que é um SO?' + ' </strong>' +
-						'</p>' +
+				'<ul>' +
+					'<li>Saudação</li>' +
+					'<li>Exemplos de SO</li>' +
+					'<li>Funçoes do SO</li>' +
+					'<li>Definição de Threade</li>' +
+					'<li>Definição de Escalonamento</li>' +
+					'<li>Definição de Escalonamento preemptivo e não preemptivo</li>' +
+					'<li>Definição de Semáforo</li>' +
+					'<li>Estados do Processo</li>' +
+					'<li>Definição de SO</li>' +
+					'<li>Definição de Deadlock</li>' +
+					'<li>Definição de Memória Virtual</li>' +
+					'<li>Definição de Paginação</li>' +
+					'<li>Definição de Segmentação</li>' +
+			    '</ul>' +
           '</div>' +
         '</div>' +
       '</div>' +
